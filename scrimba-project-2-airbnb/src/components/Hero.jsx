cceca0c2f4aed7977fc7b5d33b43590ea7979159
Bhavik <15388512+bhavik-knight@users.noreply.github.com>
2023-03-07 03:52:26 -0400

    completed react project-2 airbnb

---
+++ scrimba-project-2-airbnb/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+function Hero() {
+    return (
+        <>
+            <div className="hero-content">
+                <img src="/images/photo-grid.png" />
+            </div>
+            <div className="hero-text">
+                <h1>Online Experiences</h1>
+                <p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+            </div>
+        </>
+    )
+}
+
+export { Hero };
